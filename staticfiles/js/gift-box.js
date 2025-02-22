b0f351e41576be71cd0bedd4dcea9e7a59fedf34
gerbil1511 <gerbil1511@gmail.com>
2025-02-22 19:57:42 +0000

    refactor gift box styles and JavaScript functionality; remove unused images

--- staticfiles/js/gift-box.js
+++ staticfiles/js/gift-box.js
@@ -1,31 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log('DOM fully loaded'); // Check if the DOM is ready
 
-    const click = document.querySelector('.gift-container');
-    const lid = document.querySelector('.lid');
-    const giftBox = document.querySelector('.gift-box');
-    const shadow = document.querySelector('.shadow');
-    const surprise = document.querySelector('.surprise');
-    const text = document.querySelector('.text');
+const giftContainer = document.querySelector('.gift-container');
+const lid = document.querySelector('.lid');
+const giftBox = document.querySelector('.gift-box');
+const shadow = document.querySelector('.shadow');
+const surprise = document.querySelector('.surprise');
+const text = document.querySelector('.text');
 
-    console.log ('Javascript running');
+console.log ('Javascript running');
 
-    click.addEventListener('click', () => {
-        console.log('Gift container clicked');
-        if (click.className === "gift-container") {
-            click.classList.add('active');
-            lid.classList.add('active');
-            giftBox.classList.add('active');
-            shadow.classList.add('active');
-            surprise.classList.add('active');
-            text.classList.add('active');
-        } else {
-            click.classList.remove('active');
-            lid.classList.remove('active');
-            giftBox.classList.remove('active');
-            shadow.classList.remove('active');
-            surprise.classList.remove('active');
-            text.classList.remove('active');
-        }
+giftContainer.addEventListener('click', () => {
+
+    console.log('Gift container clicked');  //check when container is clicked
+
+    const isActive = giftContainer.classList.contains('active');
+    giftContainer.classList.toggle('active', !isActive);
+    lid.classList.toggle('active', !isActive);
+    giftBox.classList.toggle('active', !isActive);
+    shadow.classList.toggle('active', !isActive);
+    surprise.classList.toggle('active', !isActive);
+    text.classList.toggle('active', !isActive);
     });
 });
